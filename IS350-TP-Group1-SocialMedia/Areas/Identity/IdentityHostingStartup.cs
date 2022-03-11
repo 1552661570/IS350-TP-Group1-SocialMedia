f7dda20fa9936c3dac5b440b3229cf3fbb84d2d1
1552661570 <1552661570@qq.com>
2022-03-10 23:48:29 -0800

    加入完整的登录

---
+++ IS350-TP-Group1-SocialMedia/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IS350_TP_Group1_SocialMedia.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IS350_TP_Group1_SocialMedia.Areas.Identity.IdentityHostingStartup))]
+namespace IS350_TP_Group1_SocialMedia.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
